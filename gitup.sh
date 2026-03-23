db8cbce94482b10a78dbc76a704bf6616f4b6910
Collin Andrews <pizzaTrucks@outlook.com>
2026-03-23 16:22:31 -0500

    Update COMET

--- gitup.sh
+++ gitup.sh
@@ -9,13 +9,8 @@ git add .
 #echo -e '\n$ git commit -m "Merge remote changes"'
 #git commit -m "Merge remote changes"
 
-echo -e '\n$ git commit -m "Initial commit"'
-git commit -m "Initial commit"
-
-
-echo -e '\n$ git remote add origin git@github.com:Collinformatics/COMET'
-git remote add origin git@github.com:Collinformatics/COMET
-
+echo -e '\n$ git commit -m "Update COMET"'
+git commit -m "Update COMET"
 
 echo -e '\n$ git push -u origin main'
 git push -u origin main
