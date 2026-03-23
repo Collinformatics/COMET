100e7b15ec6d588d297e870635252439b68064c3
Collin Andrews <pizzaTrucks@outlook.com>
2026-03-23 16:19:37 -0500

    Initial commit

--- gitup.sh
+++ gitup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-echo -e '\n$ git pull origin'
-git pull origin main
+#echo -e '\n$ git pull origin'
+#git pull origin main
 
 echo -e '\n$ git add .'
 git add .
 
-echo -e '\n$ git commit -m "Merge remote changes"'
-git commit -m "Merge remote changes"
+#echo -e '\n$ git commit -m "Merge remote changes"'
+#git commit -m "Merge remote changes"
 
-echo -e '\n$ git push origin main'
-git push origin main
+echo -e '\n$ git commit -m "Initial commit"'
+git commit -m "Initial commit"
+
+echo -e '\n$ git push -u origin main'
+git push -u origin main
