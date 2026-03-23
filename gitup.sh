9c3850257c580ec506bc76ef5d5bbebde47f968c
Collin Andrews <pizzaTrucks@outlook.com>
2026-03-23 17:01:56 -0500

    Merge remote changes

--- gitup.sh
+++ gitup.sh
@@ -14,7 +14,11 @@ git commit -m "Merge remote changes"
 echo -e '\n$ git push origin main'
 git push origin main
 
-
+echo -e '\n$ git checkout update'
 git checkout update
+
+echo -e '\n$ git pull origin main --rebase'
 git pull origin main --rebase
+
+echo -e '\n$ git push origin update'
 git push origin update
